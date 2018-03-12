add72afd971ae96210a904260990f56400452b09
Hendi Santika <hendisantika@gmail.com>
2018-03-12 20:39:26 +0700

    #3 - Add API Binding

---
+++ src/main/java/com/hendisantika/facebooksecurity5/api/ApiBinding.java
@@ -0,0 +1,53 @@
+package com.hendisantika.facebooksecurity5.api;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : facebook-security5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/03/18
+ * Time: 20.37
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class ApiBinding {
+
+    protected RestTemplate restTemplate;
+
+    public ApiBinding(String accessToken) {
+        this.restTemplate = new RestTemplate();
+        if (accessToken != null) {
+            this.restTemplate.getInterceptors().add(getBearerTokenInterceptor(accessToken));
+        } else {
+            this.restTemplate.getInterceptors().add(getNoTokenInterceptor());
+        }
+    }
+
+    private ClientHttpRequestInterceptor getBearerTokenInterceptor(String accessToken) {
+        return new ClientHttpRequestInterceptor() {
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] bytes, ClientHttpRequestExecution execution) throws IOException {
+                request.getHeaders().add("Authorization", "Bearer " + accessToken);
+                return execution.execute(request, bytes);
+            }
+        };
+    }
+
+    private ClientHttpRequestInterceptor getNoTokenInterceptor() {
+        return new ClientHttpRequestInterceptor() {
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] bytes, ClientHttpRequestExecution execution) {
+                throw new IllegalStateException("Can't access the Facebook API without an access token");
+            }
+        };
+    }
+
+}
