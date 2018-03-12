7b8f6e0d6870331c9097d4ac1d9f64c37cec782a
Hendi Santika <hendisantika@gmail.com>
2018-03-12 20:42:29 +0700

    #4 - Add Profile Entity

---
+++ src/main/java/com/hendisantika/facebooksecurity5/api/facebook/Profile.java
@@ -0,0 +1,20 @@
+package com.hendisantika.facebooksecurity5.api.facebook;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : facebook-security5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/03/18
+ * Time: 20.42
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Data
+public class Profile {
+    private String id;
+    private String name;
+}
