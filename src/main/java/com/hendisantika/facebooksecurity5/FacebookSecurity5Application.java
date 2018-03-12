92afe3d27116c386d120fdefc47e04bd2aa20b14
Hendi Santika <hendisantika@gmail.com>
2018-03-12 20:33:31 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/facebooksecurity5/FacebookSecurity5Application.java
@@ -0,0 +1,12 @@
+package com.hendisantika.facebooksecurity5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookSecurity5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookSecurity5Application.class, args);
+    }
+}
