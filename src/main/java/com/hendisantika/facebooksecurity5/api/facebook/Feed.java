1643f85f66ce48766f104b8c6f72349256e89817
Hendi Santika <hendisantika@gmail.com>
2018-03-12 20:43:55 +0700

    #6 - Add Feed Entity

---
+++ src/main/java/com/hendisantika/facebooksecurity5/api/facebook/Feed.java
@@ -0,0 +1,22 @@
+package com.hendisantika.facebooksecurity5.api.facebook;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : facebook-security5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/03/18
+ * Time: 20.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class Feed {
+
+    private List<Post> data;
+
+}
